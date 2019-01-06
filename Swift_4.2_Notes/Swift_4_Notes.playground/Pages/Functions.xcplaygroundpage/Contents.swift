bee802f2efa09fd15111e50b22bcb07d85c4bb20
James Calby <JamesCalby@gmail.com>
2019-01-05 20:06:36 -0500

    Added NOtes

--- Swift_4.2_Notes/Swift_4_Notes.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ Swift_4.2_Notes/Swift_4_Notes.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -7,15 +7,17 @@
  */
 
 // Imputs for the function below
-let areaOfFirstRoom = area(lenght: 10, width: 12)
-let areaOfSecondRoom = area(lenght: 15, width: 15)
 
-func area(lenght: Int, width: Int) -> Int {
+
+func area(length: Int, width: Int) -> Int {
     
-    let areaOfRoom = lenght * width
+    let areaOfRoom = length * width
     
     return areaOfRoom
 }
+
+let areaOfFirstRoom = area(length: 10, width: 12)
+let areaOfSecondRoom = area(length: 15, width: 15)
 
 // Code Challenge
 //
@@ -27,8 +29,8 @@ func greeting() {
 //
 
 func  greet ( person : String ) ->  String {
-    Let greeting =  " Hello, "  + person +  " ! "
-    Return greeting
+    let greeting =  " Hello, "  + person +  " ! "
+    return greeting
 }
 
 print ( greet ( person : " Anna " ))
@@ -63,12 +65,14 @@ remove(havingValue: "A")
 // func somefunction() -> Void {}
 // Argument labels : Example
 //
-func  greetAgain ( Person : String ) ->  String {
-    Return  " Hello again, "  + person +  " ! "
+func  greetAgain ( person : String ) ->  String {
+    return  "Hello again,"  + person +  "!"
 }
-print ( greetAgain ( person : " Anna " ))
+print ( greetAgain ( person : "Anna" ))
 
-// Argument labels : Example
+
+
+// Argument labels : Code Challenge
 //
 func getRemainder(value a: Int, divisor b: Int) -> Int {
     return a % b
@@ -76,11 +80,53 @@ func getRemainder(value a: Int, divisor b: Int) -> Int {
 }
 let result = getRemainder(value: 10, divisor: 3)
 
+
+
+// default values
+//
+func carpetCostHaving(length: Int, width: Int, carpetColour colour: String = "tan") -> (price: Int, carpetColour: String) { // Returning more than one value from a function
+    
+    // Grey carpet is £1/sq ft
+    // Tan carpet is £2/sq ft
+    // Deep blue carpet is £4/sq ft
+    
+    let areaOfRoom = area(length: length, width: width)
+    // call second function area, pass through arguments from function carpetCostHaving
+    
+    var price = 0
+    
+    switch colour {
+    case "grey": price = areaOfRoom * 1
+    case "tan": price = areaOfRoom * 2
+    case "blue": price = areaOfRoom * 4
+    default: price = 0
+    }
+    return (price, colour)
+    
+}
+
+let resultOne = carpetCostHaving(length: 10, width: 20)
+
+resultOne.price
+resultOne.carpetColour
+
+
 // Code Challenge
-
-
-
-
+func coordinates (for location: String) -> (Double, Double) {
+    
+    let eifelTower: (Double, Double) = (48.8582, 2.2945)
+    let greatPyramid: (Double, Double) = (29.9792, 31.1344)
+    let sydneyOperaHouse: (Double, Double) = (33.8587, 151.2140)
+    
+    switch location {
+    case "Eiffel Tower": return eifelTower
+    case "Great Pyramid": return greatPyramid
+    case "Sydney Opera House": return sydneyOperaHouse
+        
+    default: return (0,0)
+    }
+    
+}
 
 
 
