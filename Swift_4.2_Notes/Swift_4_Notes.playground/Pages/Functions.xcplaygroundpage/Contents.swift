8dad26276eb9c6d51fcb6615f90ec4f8dd1cd903
James Calby <JamesCalby@gmail.com>
2019-01-05 19:30:56 -0500

    Added notes

--- Swift_4.2_Notes/Swift_4_Notes.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ Swift_4.2_Notes/Swift_4_Notes.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -34,25 +34,47 @@ func  greet ( person : String ) ->  String {
 print ( greet ( person : " Anna " ))
 print ( greet ( person : " Brian " ))
 
+
+
+
+// Code Challenge
+//
+
+func temperatureInFahrenheit(temperature: Double) -> Double {
+    let fahrenheit = (temperature * 9 / 5 + 32)
+    return fahrenheit
+}
+let fahrenheitTemp = temperatureInFahrenheit(temperature: 24.0)
+
+
 /*
  note
  The first actual argument to the function print(_:separator:terminator:) has no label, and its other actual arguments are optional because they all have default values. The changes to the syntax of these functions are discussed in the section on actual parameter labels and formal parameter names and the default form parameter values ​​in the lower function.
  */
 
 // Argument labels
-func remove(havingValue value: String) {
+//
+func remove(havingValue value :String) {
     print(value)
 }
-
 remove(havingValue: "A")
 
-// func somefunction() -> Void {}
 
+// func somefunction() -> Void {}
+// Argument labels : Example
+//
 func  greetAgain ( Person : String ) ->  String {
     Return  " Hello again, "  + person +  " ! "
 }
 print ( greetAgain ( person : " Anna " ))
 
+// Argument labels : Example
+//
+func getRemainder(value a: Int, divisor b: Int) -> Int {
+    return a % b
+    
+}
+let result = getRemainder(value: 10, divisor: 3)
 
 // Code Challenge
 
